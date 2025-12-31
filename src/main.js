d4b0c7012037115c44c6aa8bc19ecf66b3248d8e
Braeden Kilburn <braedenk99@gmail.com>
2025-12-31 13:00:57 -0800

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { intervalToDuration, formatDuration } from 'date-fns'
+
+const birthDate = new Date('2025-12-15T20:21:00-08:00')
+const timerElement = document.querySelector('#age-timer')
+
+function updateTimer() {
+  const duration = intervalToDuration({
+    start: birthDate,
+    end: new Date(),
+  })
+
+  const ageString = formatDuration(duration, { delimiter: ', ' })
+  timerElement.innerText = `I'm ${ageString} old`
+}
+
+// Update every second
+setInterval(updateTimer, 1000)
+updateTimer()
