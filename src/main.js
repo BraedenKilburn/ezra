3bbf45badb6ec33221d39ab4bda83ad110c60c2b
Braeden Kilburn <braedenk99@gmail.com>
2026-01-21 15:52:31 -0800

    feat: add weeks display toggle

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import './style.css'
-import { intervalToDuration, formatDuration } from 'date-fns'
+import { intervalToDuration, formatDuration, differenceInDays, differenceInWeeks, add } from 'date-fns'
 
 const birthDate = new Date('2025-12-15T20:21:00-08:00')
 const timerElement = document.querySelector('#age-timer')
+
+let showWeeksAndDays = false
+
+function getWeeksAndDaysString() {
+  const today = new Date()
+  const weeks = differenceInWeeks(today, birthDate)
+
+  const remainingDays = add(birthDate, { weeks: weeks })
+  const days = differenceInDays(today, remainingDays)
+
+  return formatDuration({ weeks, days }, { delimiter: ' and ' })
+}
 
 function updateTimer() {
   const duration = intervalToDuration({
@@ -10,9 +22,18 @@ function updateTimer() {
     end: new Date(),
   })
 
-  const ageString = formatDuration(duration, { delimiter: ', ' })
+  const ageString = showWeeksAndDays
+    ? getWeeksAndDaysString()
+    : formatDuration(duration, { delimiter: ', ' })
+
   timerElement.innerText = ageString
 }
+
+timerElement.addEventListener('click', () => {
+  showWeeksAndDays = !showWeeksAndDays
+  updateTimer()
+})
+
 updateTimer()
 
 // Update every second
