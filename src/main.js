959adc07d0d151fd6920bda5e62b3fb896df4ea1
Braeden Kilburn <braedenk99@gmail.com>
2026-01-21 16:05:10 -0800

    feat: add days option

--- src/main.js
+++ src/main.js
@@ -1,40 +1,53 @@
 import './style.css'
-import { intervalToDuration, formatDuration, differenceInDays, differenceInWeeks, add } from 'date-fns'
+import {
+  intervalToDuration,
+  formatDuration,
+  differenceInDays,
+  differenceInWeeks,
+  add,
+} from 'date-fns'
 
 const birthDate = new Date('2025-12-15T20:21:00-08:00')
-const timerElement = document.querySelector('#age-timer')
 
-let showWeeksAndDays = false
+const MODES = ['FULL', 'WEEKS_AND_DAYS', 'DAYS']
+let currentMode = 0
 
-function getWeeksAndDaysString() {
-  const today = new Date()
-  const weeks = differenceInWeeks(today, birthDate)
+const getModeStrings = {
+  FULL: () => {
+    const duration = intervalToDuration({
+      start: birthDate,
+      end: new Date(),
+    })
+    return formatDuration(duration, { delimiter: ', ' })
+  },
 
-  const remainingDays = add(birthDate, { weeks: weeks })
-  const days = differenceInDays(today, remainingDays)
+  WEEKS_AND_DAYS: () => {
+    const now = new Date()
+    const weeks = differenceInWeeks(now, birthDate)
 
-  return formatDuration({ weeks, days }, { delimiter: ' and ' })
+    const remainingDays = add(birthDate, { weeks })
+    const days = differenceInDays(now, remainingDays)
+
+    return formatDuration({ weeks, days }, { delimiter: ' and ' })
+  },
+
+  DAYS: () => {
+    const days = differenceInDays(new Date(), birthDate)
+    return formatDuration({ days })
+  },
 }
 
+const timerElement = document.querySelector('#age-timer')
+
 function updateTimer() {
-  const duration = intervalToDuration({
-    start: birthDate,
-    end: new Date(),
-  })
-
-  const ageString = showWeeksAndDays
-    ? getWeeksAndDaysString()
-    : formatDuration(duration, { delimiter: ', ' })
-
-  timerElement.innerText = ageString
+  const mode = MODES[currentMode]
+  timerElement.textContent = getModeStrings[mode]()
 }
 
 timerElement.addEventListener('click', () => {
-  showWeeksAndDays = !showWeeksAndDays
+  currentMode = (currentMode + 1) % MODES.length
   updateTimer()
 })
 
 updateTimer()
-
-// Update every second
 setInterval(updateTimer, 1000)
